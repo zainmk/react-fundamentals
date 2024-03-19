26c913a3d2035d4c0ed6aa9546293952219f62a1
zainmk <zain.khan@heliolytics.com>
2024-03-19 16:51:10 -0400

    exercise07

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -11,6 +11,7 @@ const allItems = [
 ]
 
 function App() {
+
   const [items, setItems] = React.useState(allItems)
 
   function addItem() {
@@ -30,7 +31,7 @@ function App() {
       <ul>
         {items.map(item => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
@@ -39,6 +40,7 @@ function App() {
       </ul>
     </div>
   )
+
 }
 
 export default App
